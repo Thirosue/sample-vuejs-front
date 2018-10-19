cf5e4bc203e75f59e6fd0d327e30d170ea44b63c
Takeshi Hirosue <takeshi.hirosue@bigtreetc.com>
2018-10-19 20:46:44 +0900

    fix: 本番展開用のコードだとダウンロードファイルが文字化けする

--- src/module/download.js
+++ src/module/download.js
@@ -1,40 +1,38 @@
-import encoding from 'encoding-japanese'
+import {TextEncoder} from 'text-encoding'
 
-const createFile = async (name, mimeType, model, json) => {
+const createFile = (name, mimeType, model, json) => {
   // 文字コード→SJIS、改行コード→CRLF、項目タイトルあり、BOM→無し
   const header = model.map(m => m.value)
   const headerKey = model.map(m => m.key)
   const body = []
-  json.forEach(data => body.push(headerKey.map(key => data[key]).join(',')))
-  const tmpfile = new Blob([header + "\r\n" + body.join('\r\n')], { type: mimeType });
+  json.forEach(data => body.push(headerKey.map(key => data[key]).join(',')));
+  const data = header + "\r\n" + body.join('\r\n')
 
-  //SJIS変換
-  const reader = new FileReader();
-  reader.onload = (event) => {
-    const codes = new Uint8Array(event.target.result)
-    const codeArray = encoding.convert(codes, 'SJIS') //to SJIS
-    const blob = new Blob([new Uint8Array(codeArray)], { type: mimeType })
+  const blob = new Blob([new TextEncoder("sjis", {NONSTANDARD_allowLegacyEncoding:true}).encode(data)], { type: mimeType })  //to SJIS
+  const a = document.createElement("a");
+  a.download = name;
+  a.target = "_blank";
 
-    const a = document.createElement("a")
-    a.download = name
-    a.target = "_blank"
-  
-    if (window.navigator.msSaveBlob) {
-      // for IE
-      window.navigator.msSaveBlob(blob, name)
-    } else if (window.URL && window.URL.createObjectURL) {
-      // for Firefox
-      a.href = window.URL.createObjectURL(blob)
-      document.body.appendChild(a)
-      a.click()
-      document.body.removeChild(a)
-    } else if (window.webkitURL && window.webkitURL.createObject) {
-      // for Chrome
-      a.href = window.webkitURL.createObjectURL(blob)
-      a.click()
-    }
+  if (window.navigator.msSaveBlob) {
+    // for IE
+    window.navigator.msSaveBlob(blob, name);
+  } else if (window.URL && window.URL.createObjectURL) {
+    // for Firefox
+    a.href = window.URL.createObjectURL(blob);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  } else if (window.webkitURL && window.webkitURL.createObject) {
+    // for Chrome
+    a.href = window.webkitURL.createObjectURL(blob);
+    a.click();
+  } else {
+    // for Safari
+    window.open(
+      "data:" + mimeType + ";base64," + window.Base64.encode(body),
+      "_blank"
+    );
   }
-  reader.readAsArrayBuffer(tmpfile)
 }
 
 export default {
