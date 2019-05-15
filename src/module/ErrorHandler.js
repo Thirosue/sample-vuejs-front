07d13ee1bec52806e93bd4e924ae5ea271dc5f5a
Takeshi Hirosue <mirrored1976@gmail.com>
2019-05-16 06:42:07 +0900

    fix: refs #11 エラーハンドリング修正漏れ

--- src/module/ErrorHandler.js
+++ src/module/ErrorHandler.js
@@ -1,11 +1,6 @@
-import router from '@/router';
-import store, { MUTATION_TYPES } from '@/store';
-import Config from '@/conf/Config';
-
 export default class ErrorHandler {
   static apiHandleErr(error) {
-    store.commit(MUTATION_TYPES.SET_STACKTRACE, error);
     console.error(error);
-    router.push(Config.SYSERR_PATH);
+    throw new Error(error);
   }
 }
